fc3040645a67e923d4a08c556ba8432aaef6b86e
Gunjuzone <61184208+Gunjuzone@users.noreply.github.com>
2021-06-29 23:33:47 +0100

    create pop up pagew

--- index.js
+++ index.js
@@ -11,41 +11,30 @@ function closeMenu() {
 openMenu();
 closeMenu();
 
-const popUpArray = [
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '001',
-  },
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '002',
-  },
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '003',
-  },
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '004',
-  },
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '005',
-  },
-  {
-    name: 'Mobile post stories',
-    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    skills: ['html', 'Bootstrap', 'Ruby'],
-    id: '006',
-  },
-];
+const modalBtn = document.querySelector(".modal-btn")
+const data = document.querySelector(".modal")
+const dataValue = document.createElement("div")
+dataValue.classList.add("pop-up")
+dataValue.innerHTML = `
+<h3 class="pop-up-heading">Multi Post Story</h3>
+<i class="fa fa-times-circle pop-up-close"></i>
+<ul class="pop-up-skills">
+  <li class="pop-up-skls">HTML</li>
+  <li class="pop-up-skls">CSS</li>
+  <li class="pop-up-skls">Ruby on Rails</li>
+</ul>
+<img src="Assets/Snapshoot Portfolio.png" class="pop-up-background" alt="Pop-up Image">
+<p class="pop-up-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+   when an unknown printer took a galley of type and scrambled it to make a type 
+   specimen book. It has survived not only five centuries, but also the leap into 
+   electronic typesetting, remaining essent</p>
+  <div class="pop-up-button">
+    <a href="#" class="pop-up-btn">Livelink<i class="fa fa-arrow-circle-down"></i></a>
+    <a href="#" class="pop-up-btn">Sourcelink<i class="fa fa-github"></i></a>
+  </div>`
+data.appendChild(dataValue);
+modalBtn.addEventListener("click", function(){
+dataValue.classList.toggle("displayModal")
+
+});
