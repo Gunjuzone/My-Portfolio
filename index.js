815d91efb10ca3571667ac45ff4843b14f5beb1a
Gunjuzone <61184208+Gunjuzone@users.noreply.github.com>
2021-06-29 17:46:21 +0100

    creating pop up content

--- index.js
+++ index.js
@@ -10,3 +10,42 @@ function closeMenu() {
 
 openMenu();
 closeMenu();
+
+const popUpArray = [
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '001',
+  },
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '002',
+  },
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '003',
+  },
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '004',
+  },
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '005',
+  },
+  {
+    name: 'Mobile post stories',
+    details: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    id: '006',
+  },
+];
