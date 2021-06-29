9b40a6eee4bb282d7c124636339a65909fb7cd5c
ayebaishmo <ishmo255super@gmail.com>
2021-06-28 17:05:21 -0700

    Add javascript function for the side nav menu

---
+++ index.js
@@ -0,0 +1,13 @@
+var sideMenu = document.querySelector('.nav-response');
+
+function openMenu() {
+    
+    sideMenu.style.visibility = 'visible';
+}
+
+function closeMenu() {
+    sideMenu.style.visibility = 'hidden';
+}
+
+openMenu();
+closeMenu();
