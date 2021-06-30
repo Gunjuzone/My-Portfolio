7ba06bec649063d28ec70d362b6c7326785432fd
Gunjuzone <61184208+Gunjuzone@users.noreply.github.com>
2021-06-30 22:14:17 +0100

    Using array to store pop up info

--- index.js
+++ index.js
@@ -11,43 +11,97 @@ function closeMenu() {
 openMenu();
 closeMenu();
 
-const modalBtn = document.querySelector('.modal-btn');
+// Modal Functionality
 
-const data = document.querySelector('.modal');
+data = [
+  {
+    id: 1,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+  {
+    id: 2,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+  {
+    id: 3,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+  {
+    id: 4,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+  {
+    id: 5,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+  {
+    id: 6,
+    title: 'Multi Post Stories',
+    skills: ['html', 'Bootstrap', 'Ruby'],
+    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    livelink: 'https://gunjuzone.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/Gunjuzone/My-Portfolio',
+  },
+]
 
+const modal = document.querySelector('.modal');
 const dataValue = document.createElement('div');
+const modalBtns = document.querySelectorAll('.modal-btn');
 
-dataValue.classList.add('pop-up');
-
-dataValue.innerHTML = `
-<h3 class="pop-up-heading">Multi Post Story</h3>
-<i class="fa fa-times-circle pop-up-close"></i>
-<ul class="pop-up-skills">
-  <li class="pop-up-skls">HTML</li>
-  <li class="pop-up-skls">CSS</li>
-  <li class="pop-up-skls">Ruby on Rails</li>
-</ul>
-<div class="pop-up-container">
-<img src="Assets/Snapshoot Portfolio.png" class="pop-up-background" alt="Pop-up Image">
-<div class="pop-up-right">
-<p class="pop-up-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-   when an unknown printer took a galley of type and scrambled it to make a type 
-   specimen book. It has survived not only five centuries, but also the leap into 
-   electronic typesetting, remaining essent</p>
-  <div class="pop-up-button">
-    <a href="#" class="pop-up-btn">Livelink<i class="fa fa-arrow-circle-down"></i></a>
-    <a href="#" class="pop-up-btn">Sourcelink<i class="fa fa-github"></i></a>
-    </div>
-    </div>
-    </div>`;
-data.appendChild(dataValue);
-
-const popClose = document.querySelector('.pop-up-close');
-popClose.addEventListener('click', () => {
-  dataValue.classList.add('displayModal');
+modalBtns.forEach((modalBtn) => {
+  modalBtn.addEventListener('click', openModal)
 });
 
-modalBtn.addEventListener('click', () => {
-  dataValue.classList.remove('displayModal');
-});
+function closeModal() {
+  modal.removeChild(dataValue)
+}
+
+function openModal(e) {
+  let num = (e.target.getAttribute('data-id').slice(2)) - 1;
+  const {title, skills, livelink, sourcelink, info} = data[num]
+
+  dataValue.classList.add('pop-up');
+  
+  dataValue.innerHTML = `
+  <h3 class="pop-up-heading">${title}</h3>
+  <i class="fa fa-times-circle pop-up-close"></i>
+  <ul class="pop-up-skills">
+    <li class="pop-up-skls">${skills[0]}</li>
+    <li class="pop-up-skls">${skills[1]}</li>
+    <li class="pop-up-skls">${skills[2]}</li>
+  </ul>
+  <div class="pop-up-container">
+  <img src="Assets/Snapshoot Portfolio.png" class="pop-up-background" alt="Pop-up Image">
+  <div class="pop-up-right">
+  <p class="pop-up-text">${info}</p>
+    <div class="pop-up-button">
+      <a href=${livelink} class="pop-up-btn">Livelink<i class="fa fa-arrow-circle-down"></i></a>
+      <a href=${sourcelink} class="pop-up-btn">Sourcelink<i class="fa fa-github"></i></a>
+      </div>
+      </div>
+      </div>`;
+    modal.appendChild(dataValue);
+
+    const popClose = document.querySelector('.pop-up-close');
+    popClose.addEventListener('click', closeModal);
+}
